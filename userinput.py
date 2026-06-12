761f80926edb5d3e17bee1e3985cd756c9e077d9
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-12 20:34:08 +0530

    user input

---
+++ userinput.py
@@ -0,0 +1,8 @@
+# a=input("Enter your name: ")
+# print("My name is:",a)
+
+
+b=input("Enter first number: ")
+c=input("Enter second number: ")        #Every single input is taken as string in python
+print(b+c)                      #Concatination of two strings
+print(int(b)+int(c))
