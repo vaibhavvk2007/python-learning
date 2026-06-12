0f361f0729bada775518d31eec0d1992f0f6c0f7
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-12 20:21:19 +0530

    type casting (implicit and explicit)

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+a="1"
+b="2"
+print("String",a+b)
+print("Integer",int(a)+int(b)) 
+
+#Example
+
+string="15"
+number=7
+string_number=int(string)
+sum=number+string_number
+print("The sum is: ",sum)
+
+#Implicit typecasting example
+x=5
+y=10.5
+z=x+y
+print("The sum is: ",z)
+print("The type of x is: ",type(x))
+print("The type of y is: ",type(y))
+print("The type of z is: ",type(z))
