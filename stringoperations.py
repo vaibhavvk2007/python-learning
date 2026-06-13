3e409b93d3834a62018c81154502456a8b6b6d21
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-13 20:35:51 +0530

    string slice operation

---
+++ stringoperations.py
@@ -0,0 +1,15 @@
+# name="John,Jane,Doe"
+# print("length:",len(name))
+# print(name[0:4])
+
+fruit="Mango"
+len=len(fruit)
+print(len)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:])
+print(fruit[0:-2])      #python uses len(fruit)as showed in next line
+print(fruit[-2:4])
+
+name="harry"
+print(name[-4:-2])
