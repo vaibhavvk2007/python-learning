94e8b8c04e5c0f386c90b231a86abd9d5cab069e
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-13 20:17:48 +0530

    strings characteristics

---
+++ strings.py
@@ -0,0 +1,14 @@
+name="John"
+friend="Doe"
+apple="""he said "hello" to me, I said 'hi' to him"""
+print(apple)
+
+# accessing characters in a string
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+#looping through a string
+for i in name:
+    print(i)
