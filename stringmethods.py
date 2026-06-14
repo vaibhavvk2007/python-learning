53587d4572706ca106c1a6832fa9323067c005cb
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-14 20:51:32 +0530

    string Methods

---
+++ stringmethods.py
@@ -0,0 +1,27 @@
+a="Harry!! !!!!!!!! !! Jarry!!"
+print(a)
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.replace("H","J"))
+print(a.rstrip("!"))
+print(a.split(" "))
+
+blog="hello everyone, welcome to my blog" 
+print(blog.capitalize())
+print(blog.center(50))
+print(blog.endswith("!!!!"))
+print(blog.find("wel"))
+print(blog.count("o"))
+print(blog.index("wel"))
+
+print(blog.isalnum())
+print(blog.isalpha())
+print(blog.isdigit())
+print(blog.islower())
+print(blog.isupper())
+print(blog.isprintable())   #\n \t etc are not printable
+print(blog.isspace())
+print(blog.istitle())
+
+print(blog.swapcase())
