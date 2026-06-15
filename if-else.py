37f4cea71e6705ca64514eb7238707c0d0d10d06
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-15 22:00:38 +0530

    if else conditions

---
+++ if-else.py
@@ -0,0 +1,18 @@
+a=int(input("Enter your age:"))
+print("Your age is:",a)
+
+if(a>=18):
+    print("you are eligible to vote")
+    print("you are eligible to drive")
+else:
+    print("you are not eligible to vote")
+    print("you are not eligible to drive")
+ 
+#Conditional statements 
+# <=
+# >=
+# ==
+# !=
+# <
+# >
+
