ad500e9bcbfe46e0a8f56d6b3ebdeeacd597c109
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-15 22:06:35 +0530

    elif understanding

---
+++ conditions.py
@@ -0,0 +1,12 @@
+num=int(input("Enter the value of num: "))
+print("The value of num is: ",num)
+if (num<0):
+    print("the number is negative")
+elif(num==0):
+    print("the number is zero")
+elif(num==777):
+    print("Thala for a reason")
+else:
+    print("the number is positive")
+print("This statement is always executed")
+print("the number you choose is great!!!, very good choice")
