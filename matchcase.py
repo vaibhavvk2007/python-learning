26589bd5c23c49fd30fb717d7f9c27e12ff3c952
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-17 21:05:41 +0530

    example of switch case DAY#16

--- matchcase.py
+++ matchcase.py
@@ -5,11 +5,19 @@
 
             #switch case 
 
-x=int(input("Enter a number: "))
-match x:
-    case 0:
-        print("x is zero")
-    case value if value<0:
-        print("x is negative") 
-    case value if value>0:
-        print("x is positive")
+# x=int(input("Enter a number: "))
+# match x:
+#     case 0:
+#         print("x is zero")
+#     case value if value<0:
+#         print("x is negative") 
+#     case value if value>0:
+#         print("x is positive")
+
+a=input("Enter an integer or string:")
+
+match a:
+    case _ if a.isdigit():
+        print("a is an integer")
+    case str():
+        print("a is string")
