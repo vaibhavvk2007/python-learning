659aa830502c88b5d491ac326da3c0e5f72717f3
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-17 20:59:12 +0530

    switch case 3.10

--- matchcase.py
+++ matchcase.py
@@ -1,3 +1,15 @@
-import os
+# import os
 
-print("Hello world")
+# print("Hello world")
+# print(os.getcwd())
+
+            #switch case 
+
+x=int(input("Enter a number: "))
+match x:
+    case 0:
+        print("x is zero")
+    case value if value<0:
+        print("x is negative") 
+    case value if value>0:
+        print("x is positive")
