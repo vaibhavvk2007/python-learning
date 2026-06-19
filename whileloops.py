41527cb5a456fa973151e776bc6d56349e71139a
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-19 20:49:51 +0530

    1)previous one iterations with user input 2) while loop with else block

--- whileloops.py
+++ whileloops.py
@@ -7,9 +7,16 @@
 #     i=i+1
 # print("Done with loop")
 
-i=int(input("Enter a number: "))
-while(i<=30):
-    i=int(input("Enter a number: "))
+# i=int(input("Enter a number: "))
+# while(i<=30):
+#     i=int(input("Enter a number: "))
    
     
-print("Done with loop")
+# print("Done with loop")
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("in else block")
