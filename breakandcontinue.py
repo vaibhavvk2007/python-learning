169fb3572098c09b1aca3eb36d827f57ec31a308
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-20 20:27:29 +0530

    continue statements

--- breakandcontinue.py
+++ breakandcontinue.py
@@ -3,9 +3,16 @@
 #     if i == 10:
 #         break
 
+# for i in range(10):
+#     if(i==7):
+#         print("skip the iteration or this step")
+#         continue
+#     print("2 X",i+1,"=",2*i)
 
-for i in range(10):
-    if(i==7):
-        print("skip the iteration or this step")
-        continue
-    print("2 X",i+1,"=",2*i)
+for i in range(1,101,1):
+    print(i,end=" ")
+    if (i==50):
+        break
+    else: 
+        print("sheep")
+print("Good Night")
