47421ba9d78dfd06d11dbb1697fd03e66718a58a
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-20 20:32:53 +0530

    simple example for continue statement

--- breakandcontinue.py
+++ breakandcontinue.py
@@ -9,10 +9,16 @@
 #         continue
 #     print("2 X",i+1,"=",2*i)
 
-for i in range(1,101,1):
-    print(i,end=" ")
-    if (i==50):
-        break
-    else: 
-        print("sheep")
-print("Good Night")
+# for i in range(1,101,1):
+#     print(i,end=" ")
+#     if (i==50):
+#         break
+#     else: 
+#         print("sheep")
+# print("Good Night")
+
+for i in range(1,20):
+    if(i%2==0):
+        print("skip the iteration or this step")
+        continue
+    print(i)
