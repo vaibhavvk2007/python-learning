b82c1af5414da2fc1a4d3c018b246d3c570252a9
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-21 20:44:56 +0530

    placed my name using functions in python

--- functions.py
+++ functions.py
@@ -9,37 +9,42 @@
 # gmean1=(c*d)/(c+d)
 # print(gmean1)
 
-def calculategmean(a,b):
-    mean=(a*b)/(a+b)
-    print (mean)
+# def calculategmean(a,b):
+#     mean=(a*b)/(a+b)
+#     print (mean)
 
-def isgreater(a,b):
-    if(a>b):
-        print("first number is greater")
-    else:
-        print("Second number is greater")
+# def isgreater(a,b):
+#     if(a>b):
+#         print("first number is greater")
+#     else:
+#         print("Second number is greater")
 
-def issmaller(a,b):
-    if(a<b):
-        print("first number is smaller")
-    else:
-        print("Second number is smaller")
+# def issmaller(a,b):
+#     if(a<b):
+#         print("first number is smaller")
+#     else:
+#         print("Second number is smaller")
 
-def anyfunction(a,b):
-    pass
+# def anyfunction(a,b):
+#     pass
 
-a=8
-b=6
-# gmean=(a*b)/(a+b)
-# print(gmean)
-calculategmean(a,b)
-isgreater(a,b)
-issmaller(a,b)
+# a=8
+# b=6
+# # gmean=(a*b)/(a+b)
+# # print(gmean)
+# calculategmean(a,b)
+# isgreater(a,b)
+# issmaller(a,b)
 
-c=8
-d=54
-# gmean1=(c*d)/(c+d)
-# print(gmean1)
-calculategmean(c,d)
-isgreater(c,d)
-issmaller(c,d)
+# c=8
+# d=54
+# # gmean1=(c*d)/(c+d)
+# # print(gmean1)
+# calculategmean(c,d)
+# isgreater(c,d)
+# issmaller(c,d)
+
+def name(fname,lname):
+    print("Hello",fname,lname,"!")
+
+name("Vaibhav","Khatawakar")
