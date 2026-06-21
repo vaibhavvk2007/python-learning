740a2649055772701960f4abe99f659a142a03df
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-21 20:27:27 +0530

    used a function create by me

--- functions.py
+++ functions.py
@@ -1,11 +1,25 @@
 # Functions are something that we need multiple times in a program that is a single code snippet used multiple times
+# a=8
+# b=6
+# gmean=(a*b)/(a+b)
+# print(gmean)
+
+# c=17
+# d=54
+# gmean1=(c*d)/(c+d)
+# print(gmean1)
+
+def calculategmean(a,b):
+    mean=(a*b)/(a+b)
+    print (mean)
+
 a=8
 b=6
-gmean=(a*b)/(a+b)
-print(gmean)
-
-c=17
+# gmean=(a*b)/(a+b)
+# print(gmean)
+calculategmean(a,b)
+c=8
 d=54
-gmean1=(c*d)/(c+d)
-print(gmean1)
-
+# gmean1=(c*d)/(c+d)
+# print(gmean1)
+calculategmean(c,d)
