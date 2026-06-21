60467137ae84a88d4958a7af896edbc140141aa8
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-21 20:30:34 +0530

    used a function create by me

--- functions.py
+++ functions.py
@@ -13,13 +13,22 @@ def calculategmean(a,b):
     mean=(a*b)/(a+b)
     print (mean)
 
+def isgreater(a,b):
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("Second number is greater")
+
+
 a=8
 b=6
 # gmean=(a*b)/(a+b)
 # print(gmean)
 calculategmean(a,b)
+isgreater(a,b)
 c=8
 d=54
 # gmean1=(c*d)/(c+d)
 # print(gmean1)
 calculategmean(c,d)
+isgreater(c,d)
