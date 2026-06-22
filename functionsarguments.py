33b7e58d30f02f7c472dd4f1b99b77fa88eff924
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-22 20:52:58 +0530

    default arguments example

--- functionsarguments.py
+++ functionsarguments.py
@@ -3,7 +3,12 @@
 
 # average(4,6)
 
-def average(a=5,b=9):
-    print("The average is: ",(a+b)/2)
-#In python the arguments passed outside the function eventhough the arguments are passed before
-average(4,6)
+# def average(a=5,b=9):
+#     print("The average is: ",(a+b)/2)
+# #In python the arguments passed outside the function eventhough the arguments are passed before
+# average(4,6)
+
+def name(fname, mname="Jhon",lname="Whatson"):
+    print("Hello, ",fname,mname,lname)
+
+name("peter")
