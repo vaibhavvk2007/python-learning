e36334fc01375b96748da5c64f3a8f679e0d75cd
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-24 23:28:09 +0530

    datatype of an element in a list

--- lists.py
+++ lists.py
@@ -5,4 +5,13 @@
 # print(list[2])
 
 list=[100,99,"vaibhav",True]
-print("list can have all the datatypes of elements in it but it's type is always",type(list))
+# print("list can have all the datatypes of elements in it but it's type is always",type(list))
+# print(type(list))
+# print(list[len(list)-2])
+# print(type(list[2]))            #to get the type of an single element
+
+if 10 in list:
+    print("yes")
+else:
+    print("no")
+    
