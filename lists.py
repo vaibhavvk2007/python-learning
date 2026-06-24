60b0dec3a6ba35bbd81aaf1e5f608744dccf618b
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-24 23:21:44 +0530

    list indexing

--- lists.py
+++ lists.py
@@ -1,2 +1,8 @@
-list=[1,2,3,4,5]
-print(type(list))
+# list=[1,2,3,4,5]
+# print(type(list))
+
+# print(list[0])
+# print(list[2])
+
+list=[100,99,"vaibhav",True]
+print("list can have all the datatypes of elements in it but it's type is always",type(list))
