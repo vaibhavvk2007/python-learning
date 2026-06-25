142ea86eb1d0af89b3c3f5f0737c1fb8396ecb1e
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-25 23:26:14 +0530

    index jumping in python list

--- lists.py
+++ lists.py
@@ -4,18 +4,23 @@
 # print(list[0])
 # print(list[2])
 
-list=[100,99,"vaibhav",True]
+list=[100,99,"vaibhav",True,99.99,9.69]
 # print("list can have all the datatypes of elements in it but it's type is always",type(list))
 # print(type(list))
 # print(list[len(list)-2])
 # print(type(list[2]))            #to get the type of an single element
 
-if 100 in list:
-    print("yes")
-else:
-    print("no")
+# if 100 in list:
+#     print("yes")
+# else:
+#     print("no")
 
-if "vb" in "vaibhav":
-    print("yes")
-else:
-    print("no")
+# if "vb" in "vaibhav":
+#     print("yes")
+# else:
+#     print("no")
+
+print(list)
+print(list[1:])
+print(list[:3])
+print(list[1::2])
