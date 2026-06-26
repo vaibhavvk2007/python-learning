2c7ad4b47efe5005081a2f6caa304f1db8ebd8c4
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-26 23:22:19 +0530

    list methods and indexes

--- listmethods.py
+++ listmethods.py
@@ -1,16 +1,28 @@
-l=[1,2,3,4,5,6]
+# l=[1,2,3,4,5,6]
+# print(l)
+
+# l.append(100)
+# print(l)
+
+# print(l.index(1))
+# print(l.index(2))
+# print(l.index(3))
+# print(l.index(4))
+# print(l.index(5))
+# print(l.index(6))
+# l.reverse()
+# print(l)
+
+# list=[54,57,99,12,65,10]
+# print(list)
+# list.sort()
+# print(list)
+
+l=[45,6,8,6,45,88,99,98,97,99]
+# print(l.count(99))
+# print(l.count(1))
+
+m=l.copy()
+m[0]=0
 print(l)
-
-l.append(100)
-print(l)
-
-l.reverse()
-print(l)
-
-print(l.index(1))
-
-list=[54,57,99,12,65,10,]
-print(list)
-list.sort()
-print(list)
-
+print(m)
