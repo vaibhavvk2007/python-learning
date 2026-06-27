5fff8d8eef8a71b106bc3e833af604779669aa77
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-27 19:38:28 +0530

    Tuples

---
+++ tuples.py
@@ -0,0 +1,33 @@
+tup=(1,2,5,6,True,False,"hello","bye","99")
+# tup[0]=90
+
+print(type(tup),tup)
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+
+# i=5
+# j=2
+# for i in tup:
+#     for j in tup:
+#         print(j)
+# print(i)
+
+# if 99 in tup:
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+
+
+
+
+
+
+
+
+
+# t=("abc",)
+# print(type(t)) 
