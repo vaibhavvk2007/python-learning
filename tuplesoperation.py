12e04e2f6e26896ec3c8c25e763f56f732d44cbf
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-27 19:58:50 +0530

    tuple operations

--- tuplesoperation.py
+++ tuplesoperation.py
@@ -1,5 +1,6 @@
 #Tuples and it's elements cannot be changed and if needed then the tuple should be converted to a list then made changes and then again change it to a tuple as there is no in built methods to changes it.
 #Here is an Example
+#inshort Tuples Are immutable and Lists are mutable
 countries=["India","Russia","Ukarine","Usa","china","Japan"]
 temp=list(countries)
 temp.append("switzerland")
@@ -7,3 +8,12 @@ temp.pop(3)
 temp[3]="Finland"
 countries=tuple(temp)
 print(countries)
+
+
+tup1=(1,2,3,4,100,6,7,8,9)
+tup2=(10,11,12,13,14,15)
+tuple=tup1+tup2
+print(tuple)
+print(tuple.count(10))
+print(tuple.index(4))
+print(tuple.index(7))
