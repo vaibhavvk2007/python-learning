1685362042e7748e0d3aaf042557593c5962848b
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-28 23:17:10 +0530

    f-Strings

--- f-strings.py
+++ f-strings.py
@@ -4,3 +4,8 @@ letter="Hello I am {} and I am from {}"
 country="India"
 name="Vaibhav"
 print(letter.format(name,country))
+print(f"Hello I am {name} and I am from {country}")
+price=99.444444
+txt=f"For only{price: .2f} dollars"
+print(txt)
+# print(txt.format())
