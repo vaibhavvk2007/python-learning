8296af743145a2f1a3a4b257c688a9a8b9833d5e
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-29 22:19:24 +0530

    DocStrings use case and what are they used for and revision of a function

--- docStrings.py
+++ docStrings.py
@@ -4,3 +4,17 @@ def square(n):
     print(n**2)
 a=int(input("Enter a number: "))
 square(a)
+print(square.__doc__)
+
+
+def square(n):
+    '''Takes in a number and returns it's square'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+
+# def add(a,b):
+#     print(a+b)
+
+# add(5,2)
