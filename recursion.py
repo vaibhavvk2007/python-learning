592c461e31152b5f816523ecd8eb1720b4cd21d2
Vaibhav V K <VvkVvk070102@gmail.com>
2026-06-30 20:57:26 +0530

    Recursion program fibonacci series

--- recursion.py
+++ recursion.py
@@ -11,3 +11,13 @@ print("Number:",n)
 print(f"factorial of {n} is",factorial(n))
 
         #recommiting
+
+# Simple Fibonacci Series Program
+
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
