01e7e768a0c7d6ea0b82acf449aba49ebbbfac97
Vaibhav V K <VvkVvk070102@gmail.com>
2026-07-02 18:17:02 +0530

    DAY #31

--- Sets.py
+++ Sets.py
@@ -1,8 +1,15 @@
             #Day31
 
-s={1,2,3}
-print(s)
-print(type(s))
+# s={1,2,3}
+# print(s)
+# print(type(s))
 
 data={"vaibhav","sharma",19,19.6,398,True}
 print(data)
+
+for i in data:
+    print(i)
+
+# # quiz={}         Dictionary
+# quiz=set()
+# print(type(quiz))           
