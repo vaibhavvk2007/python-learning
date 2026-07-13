1cca4b344c948d1222ef488fd4a7228283132fbb
Vaibhav V K <VvkVvk070102@gmail.com>
2026-07-13 20:30:35 +0530

    symmetric difference and symmetric difference update

--- setmethods.py
+++ setmethods.py
@@ -1,6 +1,23 @@
             #DAY32
-s1={1,2,3}
-s2={3,5,6}
-print(s1.union(s2))
-s1.update(s2)
-print(s1,s2)
+# s1={1,2,3}
+# s2={3,5,6}
+# print(s1.union(s2))
+# s1.update(s2)
+# print(s1,s2)
+
+cities={"Tokyo","Madrid","Berlin","Delhi"}
+cities2={"Tokyo","Seoul","Kabul","Madrid"}
+#1      cities3=cities.intersection(cities2)        # This returns a new set  
+#       print(cities3)                      
+
+#2       cities.intersection_update(cities2)        # This updates in the existing sets
+#       print(cities)
+    #   order is not valid for all in the sets
+    #1 and 2  are intersection sections that prints or returns only the same elements
+
+# cities3=cities.symmetric_difference(cities2)        # This returns a new set  
+# print(cities3)
+
+
+# cities3=cities.symmetric_difference_update(cities2)        # This updates in the existing sets 
+# print(cities3)
